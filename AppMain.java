190235578ec9085cdd7497ecc17ba24799705471
narayana-java <narayana-java@users.noreply.github.com>
2016-09-10 22:35:21 -0400

    Content changes

--- AppMain.java
+++ AppMain.java
@@ -15,7 +15,7 @@ public class AppMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Narayana");
+        System.out.println("Hello Narayana Test");
     }
     
 }
